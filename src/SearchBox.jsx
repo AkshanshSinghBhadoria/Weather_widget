bb34aa68aec4a42a8813ec924bece228e59ef47f
Akshansh Singh Bhadoria <133026950+AkshanshSinghBhadoria@users.noreply.github.com>
2024-07-02 18:02:08 +0530

    added openWeather API

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -6,6 +6,23 @@ import { useState } from 'react';
 export default function SearchBox() {
     let [city, setCity] = useState("");
 
+    const API_URL = "https://api.openweathermap.org/data/2.5/weather";
+    const API_KEY = "58f1cd29ba3052dcd22f2915027d4bf2";
+
+    let getWeatherInfo = async () => {
+        let response = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
+        let jsonResponse = await response.json();
+        let result = {
+            temp: jsonResponse.main.temp,
+            minTemp: jsonResponse.main.temp_min,
+            maxTemp: jsonResponse.main.temp_max,
+            humidity: jsonResponse.main.humidity,
+            feelsLike: jsonResponse.main.feels_like,
+            weather: jsonResponse.weather[0].description,
+        };
+        console.log(result);
+    };
+
     let handleChange = (evt) => {
         setCity(evt.target.value);
     }
@@ -14,6 +31,7 @@ export default function SearchBox() {
         evt.preventDefault();
         console.log(city);
         setCity("");
+        getWeatherInfo();
     }
 
     return (
